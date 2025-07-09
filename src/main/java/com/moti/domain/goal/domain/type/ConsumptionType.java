1caa5b8b094a5b7996513229d3b0b83dcd6b420d
jyj1289 <jin37091289@gmail.com>
2025-07-10 02:29:31 +0900

    feat: 소비유형 추가

--- src/main/java/com/moti/domain/goal/domain/type/ConsumptionType.java
+++ src/main/java/com/moti/domain/goal/domain/type/ConsumptionType.java
@@ -7,7 +7,13 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ConsumptionType implements EnumProperty {
-    SHOPPING("쇼핑중독");
+    SAVING("절약형"),
+    INVESTMENT("투자형"),
+    FLEX("플렉서형"),
+    EXPERIENCE("경험형"),
+    FOOD("미식형"),
+    IMPROVEMENT("자기계발형"),
+    SHOPPING("쇼핑러");
 
     private final String description;
 }
