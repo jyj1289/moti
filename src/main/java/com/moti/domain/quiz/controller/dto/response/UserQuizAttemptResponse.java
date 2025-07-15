68f1b1ec558d0c9769579a6cb733017de868cc52
jyj1289 <jin37091289@gmail.com>
2025-07-15 09:59:02 +0900

    feat: Response에 createdAt 추가

--- src/main/java/com/moti/domain/quiz/controller/dto/response/UserQuizAttemptResponse.java
+++ src/main/java/com/moti/domain/quiz/controller/dto/response/UserQuizAttemptResponse.java
@@ -4,6 +4,8 @@ import com.moti.domain.quiz.domain.UserQuizAttempt;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class UserQuizAttemptResponse {
@@ -12,11 +14,13 @@ public class UserQuizAttemptResponse {
     private final Long userAnswer;
     private final Long correctAnswer;
     private final QuizResponse quiz;
+    private final LocalDateTime createdAt;
 
     public UserQuizAttemptResponse(UserQuizAttempt attempt) {
         this.isSolved = attempt.isSolved();
         this.userAnswer = attempt.getAnswer();
         this.correctAnswer = attempt.getCorrectAnswer();
         this.quiz = new QuizResponse(attempt.getQuiz());
+        this.createdAt = attempt.getCreatedAt();
     }
 }
